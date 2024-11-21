30aa6d690d4cbfdb5a398ff6a2e7fda4d41f136a
lzamora <wuizamora21@gmail.com>
2024-11-21 17:21:53 -0600

    FORMATO USUARIOS

--- src/Models/Servicios/formato_usuarios_PDF.php
+++ src/Models/Servicios/formato_usuarios_PDF.php
@@ -36,26 +36,27 @@ if (isset($_GET['solicitante']) && isset($_GET['user']) && isset($_GET['pass']))
     <head>
         <meta charset="UTF-8">
         <title>Formato usuarios</title>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
         <style>
-            body {font-family: Arial, sans-serif; margin: 0; padding: 0;}
+            body {font-family: "Roboto", sans-serif; margin: 0; padding: 0;}
             .texto {font-size: 0.9rem; padding:1rem; text-align: justify;}
-            .footer {position: fixed; bottom: 0; left: 0; width: 100%; height: 3rem; display: flex; align-items: center; padding: 0 1rem; box-sizing: border-box;}
-            .footer img { right: 1rem; bottom: 1rem; height: 2rem; object-fit: contain;}
+            .footer {position: fixed; bottom: 0; left: 0; width: 100%; height: 4rem; display: flex; align-items: center; padding: 0 1rem; box-sizing: border-box;}
+            .footer img { right: 1rem; bottom: 1rem; object-fit: contain; margin-bottom:2rem;}
         </style>
     </head>
     <body>
-        <img src="http://localhost/INFORMATICA/public/images/cabecera_completa.png" alt="LOGO CDMX" width=700>
+        <img src="http://localhost/INFORMATICA/public/images/encabezado.png" alt="LOGO CDMX" width=700>
         <p style="padding-left:23rem;">Ciudad de México, a ' . $fecha_hoy . '</p>
         <p class="texto">
-        <strong>' . $data['NombreCompleto'] . '<br>Puesto: ' . $data['Puesto'] . '<br>Usuario:</strong> ' . $user . '<br><strong>Contraseña:</strong> '. $pass . '<br><br>
+        <strong>' . $data['NombreCompleto'] . '<br>Puesto: ' . $data['Puesto'] . '</strong> ' . '<br><strong>Contraseña:</strong> '. $pass . '<br><br>
         <strong>PRESENTE</strong><br>
-        Con base en las Políticas del Instituto de Verificación Administrativa de la Ciudad de México sobre Usuarios, Contraseñas, Internet, Intranet, Correo Electrónico, Telefonía e Internet Móvil, Telefonía Local y Dispositivos Móviles, descritas de acuerdo al  aviso en la Gaceta Oficial de la Ciudad de México número 239 de fecha 12 de enero de 2018, y que pueden ser consultadas el siguiente enlace electrónico <a href"http://lto7.ddns.net/invea/informes/usuarios.php">http://lto7.ddns.net/invea/informes/usuarios.php</a> así como una implementación de seguridad para el personal de este Instituto, me permito informarle que a partir del día de hoy, le es asignado su nombre de usuario y contraseña, cuya acción permitirá a usted tener la certeza de que la contraseña es UNICA y que personal ajeno a usted no la posee. <br>
+        Con base en las Políticas del Instituto de Verificación Administrativa de la Ciudad de México sobre Usuarios, Contraseñas, Internet, Intranet, Correo Electrónico, Telefonía e Internet Móvil, Telefonía Local y Dispositivos Móviles, descritas de acuerdo al  aviso en la Gaceta Oficial de la Ciudad de México número 239 de fecha 12 de enero de 2018, y que pueden ser consultadas el siguiente enlace electrónico <a href"http://lto7.ddns.net/invea/informes/usuarios.php">http://lto7.ddns.net/invea/informes/usuarios.php</a> así como una implementación de seguridad para el personal de este Instituto, me permito informarle que a partir del día de hoy, le es asignado su nombre de usuario y contraseña, cuya acción permitirá a usted tener la certeza de que la contraseña es ÚNICA y  que personal ajeno a usted no la posee. <br>
 
         Asimismo, acepta y se compromete a cumplir con las políticas para el uso adecuado de los servicios informáticos proporcionados por el Instituto de Verificación Administrativa de la Ciudad de México en los siguientes términos:<br>
 
         1. La contraseña es intransferible. <br>
         2. Toda la actividad realizada con el usuario y contraseña será susceptible de ser auditado y/o verificado.<br>
-        3. Toda la actividad generada por este usuario en los equipos de cómputo y/o servicios de red, así como la transmisión, resguardo y modificación de datos, a partir de mi ingreso y hasta la solicitud de baja, será <strong>UNICA Y EXCLUSIVA</strong>, asumiendo toda la responsabilidad del uso de la información en los sistemas del Instituto de Verificación Administrativa de la Ciudad de México.<br>
+        3. Toda la actividad generada por este usuario en los equipos de cómputo y/o servicios de red, así como la transmisión, resguardo y modificación de datos, a partir de mi ingreso y hasta la solicitud de baja, será <strong>ÚNICA Y EXCLUSIVA</strong>, asumiendo toda la responsabilidad del uso de la información en los sistemas del Instituto de Verificación Administrativa de la Ciudad de México.<br>
         4. Por este medio certifico, que he leído y comprendo completamente el contenido del Presente, la cual valido con mi firma, misma que certifica el conocimiento, aceptación y apego a las Políticas de Usuarios, Contraseñas, Internet, Intranet, Correo Electrónico, Telefonía e Internet Móvil, Telefonía Local y Dispositivos Móviles.<br>
 
         En virtud de lo anterior su firma abajo manifiesta el conocimiento, aceptación y apego a las políticas, reglas y procedimientos de la organización en relación al usuario y contraseña que me permite el acceso a los diferentes servicios proporcionados por el Instituto de verificación Administrativa. <br>
@@ -68,8 +69,8 @@ if (isset($_GET['solicitante']) && isset($_GET['user']) && isset($_GET['pass']))
     $html .= '</div>
     <script type="text/php">
         if (isset($pdf)) {
-            $x = 490;
-            $y = 775;
+            $x = 280;
+            $y = 770;
             $text = "Página {PAGE_NUM} de {PAGE_COUNT}";
             $font = null;
             $size = 9;
@@ -77,9 +78,11 @@ if (isset($_GET['solicitante']) && isset($_GET['user']) && isset($_GET['pass']))
             $pdf->page_text($x, $y, $text, $font, $size, $color);
         }
     </script>
+    <img src="http://localhost/INFORMATICA/public/images/fcp.png" alt="Logo Felipe" width="150" height="100" style="float: right;">
+
     <div class="footer"> 
         <img src="http://localhost/INFORMATICA/public/images/direccion_invea.png" alt="Logo Footer" width=300>
-    </div>
+        </div>
     </body>
     </html>';
 
